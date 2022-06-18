e5e6da1eb502c50bc5e73bc2422b8e5a437c75c0
lucianalyma <lu_llim@hotmail.com>
2022-06-18 08:25:24 -0300

    Function

---
+++ script.js
@@ -0,0 +1,23 @@
+function msg (){
+    console.log ("É nós!")
+}
+msg ()
+
+function myName(nome){
+console.log(`Bem-vinda, ${nome}!`)
+ }
+    
+myName("Luciana!")
+
+function aboutMe(name, age, music){
+console.log (`My name is ${name}, I have ${age} years and I love to listen ${music}`)
+}
+aboutMe("Lu", 36, "MPB.")
+function fun(movie, music){
+    console.log (`My favorite film is ${movie}, and my favorite music is ${music}`)
+    }
+    fun("THE FREDOM WRITERS", "I LOVE BY GRACE.")
+function calc (triplo){
+    return triplo * 3
+}
+ console.log (calc(5))
